5a748b3262d66c38758505dfc87cee4baa5b875a
wbix <wbix@Mac-mini-admin.local>
2019-02-27 17:51:33 +0300

    modified file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
+
   def index
   	session[:conversation] ||= []
 
   	@users = User.all.where.not(id: current_user)
-  	@conversation = Conversation.includes(:recipient, :messages)
-  								.find(session[:conversation])
+  	@conversations = Conversation.includes(:recipient, :messages).find(session[:conversation])
   end
 end
--- db/migrate/20190227140638_create_messages.rb
+++ db/migrate/20190227140638_create_messages.rb
@@ -1,4 +1,5 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
+ 
   def change
     create_table :messages do |t|
       t.text :body
