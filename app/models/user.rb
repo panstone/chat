4e0f541a44dcd8d2a050dca71e5de14f132e820d
wbix <wbix@Mac-mini-admin.local>
2019-02-27 17:23:42 +0300

    create app

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :messages
+  has_many :conversations, foreign_key: :sender_id
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  
+
+end
